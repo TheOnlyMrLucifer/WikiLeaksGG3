f0314d0a10759f766a51d335bbc8ea313c019c8a
Chloe Decker <azeydutch@gmail.com>
2022-06-17 12:11:40 +0200

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -171,7 +171,7 @@ help_string_telegraph = f'''<br>
 '''
 
 help = telegraph.create_page(
-        title='Mirror-Leech-Bot Help',
+        title='WikiLeaks Leech Bot Help',
         content=help_string_telegraph,
     )["path"]
 
